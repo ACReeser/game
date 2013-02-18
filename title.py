f1c95244d9ccbe378355551586f676345daac3a5
jdreeser <jdreeser@gmail.com>
2013-02-17 23:49:41 -0800

    Create title.py

---
+++ title.py
@@ -0,0 +1,53 @@
+def printtitle():
+  print """
+                    #####  ####### #     #
+                   #     # #        #   #
+                   #       #         # #
+                    #####  #####      #
+                         # #         # #
+                   #     # #        #   #
+                    #####  ####### #     #
+
+#####   #    #  #    #   ####   ######   ####   #    #   ####
+#    #  #    #  ##   #  #    #  #       #    #  ##   #  #
+#    #  #    #  # #  #  #       #####   #    #  # #  #   ####
+#    #  #    #  #  # #  #  ###  #       #    #  #  # #       #
+#    #  #    #  #   ##  #    #  #       #    #  #   ##  #    #
+#####    ####   #    #   ####   ######   ####   #    #   ####
+
+A N D                D R A G O N S '               D I L D O S
+
+
+Written and Programmed by: John "Unnf" Reeser
+Copyright 2013 [Enter 'h' for help]
+--------------------------------------------------------------
+  """
+
+
+def roomobjects():
+  roomdict = {
+    'wimp wamp womp' : 'more',
+  }
+
+def help():
+  print """
+                        .----------.
+------------------------|SDADD HELP|--------------------------
+                        '----------'
+From the main prompt, your options are:
+
+room     ---- sends you to the test room, where you can look
+              at some things to test out the dictionary
+
+charstats --- will give you the option to look at your
+              character's stats
+
+talk      --- will give you a short description of a randomly
+              gen'rd NPC, along with stats
+
+town      --- will give you a short description of a randomly
+              gen'rd town
+
+q        ---- typing 'q' at any point will exit you from your
+              current menu
+  """
