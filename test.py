e65f982d28a13bc64c32ede71a812f0088fc6fe2
John <jack@gmail.com>
2013-02-18 02:24:52 -0600

    curses pre-work

---
+++ test.py
@@ -0,0 +1,30 @@
+import curses
+
+screen = curses.initscr()
+curses.noecho()
+curses.curs_set(0)
+screen.keypad(1)
+
+def center(x):
+   screen.addstr(12, 40 - (len(x) * 1/2), x)
+
+center("PRESS A BUTTON, MOTHERFUCKER!")
+
+while True:
+   event = screen.getch()
+   if event == ord("q"): break
+   elif event == ord("p"):
+      screen.clear()
+      center("The User Pressed Lower Case p")
+   elif event == ord("P"):
+      screen.clear()
+      center("The User Pressed Upper Case P")
+   elif event == ord("3"):
+      screen.clear()
+      center("The User Pressed 3")
+   elif event == ord(" "):
+      screen.clear()
+      center("The User Pressed The Space Bar")   
+
+curses.endwin()
+
