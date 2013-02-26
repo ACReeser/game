366b5cd71f32c2ef32800c09202067ed41b93320
John <jdreeser@gmail.com>
2013-02-26 16:44:35 -0600

    finished race bonuses in character creator

--- newcreator.py
+++ newcreator.py
@@ -92,18 +92,35 @@ def pointbuy():
   for i in range(6):
     os.system("clear")
     
+    print """
+ABILITY SCORE : POINT COST
+--------------------------
+-3             -4
+-2             -2
+-1             -1
+ 0              0
+ 1              1
+ 2              2
+ 3              3
+ 4              5
+ 5              7
+ 6              10
+ 7              13
+ 8              17
+    """
+    
     print "Points:", points
     
     while 1:
       next = raw_input(names[i] + " = ")
 
-      if next in points_l:
+      if next in points_l and int(points_l[next]) <= points:
         abils[i] += int(next)
         points -= int(points_l[next])
         break
       else:
-       print "You chose an invalid number!"
-
+        print "You chose an invalid number!"
+      
   os.system("clear")
   return abils
 
@@ -131,11 +148,11 @@ def assignabils():
     print abils
     
     while 1:
-      next = int(raw_input("> "))
+      next = raw_input("> ")
       
-      if next in abils:
-        abilsnew[i] = next
-        abils.remove(next)
+      if int(next) in abils:
+        abilsnew[i] = int(next)
+        abils.remove(int(next))
         break
       else:
         print "You picked a number that's not in that list!"
@@ -152,12 +169,115 @@ WIS = %d
 CHA = %d
   """ % (abils[0], abils[1], abils[2], abils[3], abils[4], abils[5])
 
-os.system("clear")
-print "Which method of ability score generation would you like?"
-print "Enter 'h' for an explanation ability scores"
-print "1 - Point-Buy\n2 - Standard Random Roll"
+def showhelp():
+  os.system("clear")
+  print """
+STR measures muscle and physical power
+DEX measures agility, reflexes, and balance
+CON measures health and stamina
+INT determines how well your character learns and reasons
+WIS describes a character's willpower, common sense, awareness and intuition
+CHA measures a character's personality, personal magnetism, ability to lead, and appearance
+  """
+  raw_input("PRESS ENTER")
+
+def racebonus():
+  def race_c(q,w,e,r,t,y):
+    bonus_l = (q, w, e, r, t, y)
+    for i in range(6):
+      abils[i] += bonus_l[i]
+
+    return abils
+
+  def race_p():
+    showabils()
+    print """
+Pick One Ability Score To Increase By 2
+---------------------------------------
+1 - STR
+2 - DEX
+3 - CON
+4 - INT
+5 - WIS
+6 - CHA
+    """
+    while 1:
+      next = raw_input("> ")
+      
+      if int(next) - 1 in range(6):
+        abils[int(next) - 1] += 2
+        break
+      else:
+        print "You picked an invalid number!"
+
+    return abils
+
+  def racehelp():
+    print 'TKTKTK'
+
+  race_l = ('dwarf', 'halfling', 'elf', 'human', 'gnome', 'half-orc',
+    'half-elf', 'catfolk', 'goblin', 'orc')
+
+  while 1:
+    os.system("clear")
+    
+    print """
+Race Selection
+'^^^^^__^^^^^'
+-..---::---..-
+1 - Dwarf
+2 - Halfling
+3 - Elf
+4 - Human
+5 - Gnome
+6 - Half-Orc
+7 - Half-Elf
+8 - Catfolk
+9 - Goblin
+10 - Orc
+-..---::---..-
+_vvvvv--vvvvv_
+Race Info: 'h'
+    """
+    next = raw_input("> ")
+
+    if next == "h" or next == "H":
+      racehelp()
+    elif race_l[int(next) - 1] in race_l:
+      race = race_l[int(next) - 1]
+      if race == "dwarf":
+        abilsnew = race_c(0, 0, 2, 0, 2, -2)
+      elif race == "halfling":
+        abilsnew = race_c(-2, 2, 0, 0, 0, 2)
+      elif race == "elf":
+        abilsnew = race_c(0, 2, -2, 2, 0, 0)
+      elif race == "human":
+        abilsnew = race_p()
+      elif race == "gnome":
+        abilsnew = race_c(-2, 0, 2, 0, 0, 2)
+      elif race == "half-orc":
+        abilsnew = race_p()
+      elif race == "half-elf":
+        abilsnew = race_p()
+      elif race == "catfolk":
+        abilsnew = race_c(0, 2, 0, 0, -2, 0)
+      elif race == "goblin":
+        abilsnew = race_c(-2, 4, 0, 0, 0, -2)
+      elif race == "orc":
+        abilsnew = race_c(4, 0, 0, -2, -2, -2)
+      break
+    else:
+      print "You chose an invalid number!"
+      raw_input("")
+
+  return abilsnew
 
 while 1:
+  os.system("clear")
+  print "Which method of ability score generation would you like?"
+  print "Enter 'h' for an explanation ability scores"
+  print "1 - Point-Buy\n2 - Standard Random Roll"
+
   next = raw_input("> ")
 
   if next == "1":
@@ -171,3 +291,7 @@ while 1:
     break
   elif next == "h" or next == "H":
     showhelp()
+
+os.system("clear")
+abils = racebonus()
+showabils()
