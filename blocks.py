6547d3cd6784023151d6e7d6e73203d49312385b
John <jdreeser@gmail.com>
2013-03-29 15:11:19 -0500

    code is now properly commented

--- blocks.py
+++ blocks.py
@@ -6,28 +6,33 @@ traits_orig = ['is crazy', 'loves music obsessively',
         'loves to cook',
         'hates themself', 'once owned a snake', 'is ugly', 'sucks',
         'killed a man, once', 'thinks life is pointless',
-        'loves getting fucked really hard', 'listens to porn', 'masturbates',
+        'loves getting fucked really hard', 'listens to porn',
         'hates old people', 'buried an old guy in their back yard']
 
-for k in range(3):
-            traits_orig.append(raw_input("> "))
+#for k in range(3):
+#            traits_orig.append(raw_input("> "))
 
 class drone(object):
+    # makes an object and then gives that object a random list of traits
     def __init__(self):
         self.temp = traits_orig[:]
         self.odds = []
-        for i in range(3):
+        for i in range(4):
             self.odds.append(self.temp.pop(randint(0, len(self.temp) - 1)))
 
+# makin' babies
 bob = drone()
 jane = drone()
 
+# using list comprehension to copy shared traits into a common list
 shared = [x for x in bob.odds if x in jane.odds]
 
+# shows console if any traits are shared
 for x in shared:
     print "Jane", x + ", too."
 
-if len(shared) == 3:
+# sets relationship booleans according to shared traits
+if len(shared) >= 3:
     compatible = True
     soul_mates = True
 elif len(shared) == 2:
@@ -38,6 +43,7 @@ else:
     soul_mates = False
 
 
+# prints relationship string based on relationship booleans
 if compatible and soul_mates:
     print "We're in love!"
 elif compatible:
