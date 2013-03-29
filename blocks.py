0d35661b2cdee1e772e9204a5eedf094c7cf1363
John <jdreeser@gmail.com>
2013-03-29 01:16:28 -0500

    can now add custom oddities, code also compares people and lets you know if they're compatible

--- blocks.py
+++ blocks.py
@@ -2,21 +2,45 @@ from random import randint
 
 traits_orig = ['is crazy', 'loves music obsessively', 
         'is just waiting for the end of the world', 
-        'loves you Jesus Chriiiiiiiiiiiiist', 'wants butt seks',
-        'loves to cook', 'has a little black book with his poems in',
+        'wants butt seks',
+        'loves to cook',
         'hates themself', 'once owned a snake', 'is ugly', 'sucks',
-        'killed a man, once', 'thinks life is pointless']
+        'killed a man, once', 'thinks life is pointless',
+        'loves getting fucked really hard', 'listens to porn', 'masturbates',
+        'hates old people', 'buried an old guy in their back yard']
+
+for k in range(3):
+            traits_orig.append(raw_input("> "))
 
 class drone(object):
     def __init__(self):
-        traits = traits_orig
+        self.temp = traits_orig[:]
         self.odds = []
         for i in range(3):
-            got = randint(0, len(traits) - 1)
-            self.odds.append(traits[got])
-            traits.remove(traits[got])
+            self.odds.append(self.temp.pop(randint(0, len(self.temp) - 1)))
 
 bob = drone()
-for i in range(len(bob.odds)):
-    print "Bob %s." % bob.odds[i]
+jane = drone()
 
+shared = [x for x in bob.odds if x in jane.odds]
+
+for x in shared:
+    print "Jane", x + ", too."
+
+if len(shared) == 3:
+    compatible = True
+    soul_mates = True
+elif len(shared) == 2:
+    compatible = True
+    soul_mates = False
+else:
+    compatible = False
+    soul_mates = False
+
+
+if compatible and soul_mates:
+    print "We're in love!"
+elif compatible:
+    print "We're getting along great!"
+else:
+    print "This is ok, i guess..."
