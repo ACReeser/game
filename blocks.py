db2252801f8cce3104c9d24e3c5e0e56ca1a60f9
John <jdreeser@gmail.com>
2013-03-30 02:03:10 -0500

    slowly adding mood functions

--- blocks.py
+++ blocks.py
@@ -1,6 +1,12 @@
 from random import randint
 
-traits_orig = ['is crazy', 'loves music obsessively', 
+#for k in range(3):
+#            traits_orig.append(raw_input("> "))
+
+class drone(object):
+    # makes an object and then gives that object a random list of traits
+    
+    traits_orig = ['is crazy', 'loves music obsessively', 
         'is just waiting for the end of the world', 
         'wants butt seks',
         'loves to cook',
@@ -9,27 +15,33 @@ traits_orig = ['is crazy', 'loves music obsessively',
         'loves getting fucked really hard', 'listens to porn',
         'hates old people', 'buried an old guy in their back yard']
 
-#for k in range(3):
-#            traits_orig.append(raw_input("> "))
-
-class drone(object):
-    # makes an object and then gives that object a random list of traits
-    def __init__(self):
-        self.temp = traits_orig[:]
+    def __init__(self, name):
+        self.name = name
+        self.temp = drone.traits_orig[:]
         self.odds = []
+        self.stress = 0
+        self.happy = 0
+        self.mood = 'neutral'
         for i in range(4):
             self.odds.append(self.temp.pop(randint(0, len(self.temp) - 1)))
 
+    def change_mood(self, stress_i=0, happy_i=0):
+        self.stress += stress_i
+        self.happy += happy_i
+
+    def print_mood(self):
+        print self.name, "is", self.mood + "."
+
 # makin' babies
-bob = drone()
-jane = drone()
+bob = drone('Bob')
+jane = drone('Jane')
 
 # using list comprehension to copy shared traits into a common list
 shared = [x for x in bob.odds if x in jane.odds]
 
 # shows console if any traits are shared
 for x in shared:
-    print "Jane", x + ", too."
+    print "%s %s, too." % (jane.name, x)
 
 # sets relationship booleans according to shared traits
 if len(shared) >= 3:
@@ -46,7 +58,12 @@ else:
 # prints relationship string based on relationship booleans
 if compatible and soul_mates:
     print "We're in love!"
+    bob.change_mood(-2, 5)
 elif compatible:
     print "We're getting along great!"
+    bob.change_mood(-1, 2)
 else:
     print "This is ok, i guess..."
+    bob.change_mood(0, 0)
+
+bob.print_mood()
